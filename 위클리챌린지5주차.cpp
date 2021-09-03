9f9862cdc2830ceba7eff6df840f0238f39da240
khs960616 <55014803+khs960616@users.noreply.github.com>
2021-09-04 02:50:25 +0900

    간단한 점화식
    각 자리수별 증가폭을 점화식을 이용해 계산하여 풀었다.

---
+++ 위클리챌린지5주차.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+vector<int> init_vector(){
+    vector<int> v(5,0);
+    v[4] = 1;
+    
+    for(int i=3; i>=0;i--){
+        v[i] = v[i+1] + pow(5,4-i);
+    }
+    return v;
+}
+void init_map(map<char,int> &m){
+    m.insert(make_pair('A',1));
+    m.insert(make_pair('E',2));
+    m.insert(make_pair('I',3));
+    m.insert(make_pair('O',4));
+    m.insert(make_pair('U',5));
+}
+
+int solution(string word) {
+    vector<int> v = init_vector();
+    map<char, int> m;
+    init_map(m);
+    int len = word.length();
+    int answer = 0;
+    for(int i=0;i<len;i++){
+        answer += (v[i]*(m[word[i]]-1)+1);
+    }
+    return answer;
+}
