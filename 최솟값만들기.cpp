b3ed77278c638080d4526ef791d7c2e31b4bc058
khs960616 <55014803+khs960616@users.noreply.github.com>
2021-09-07 04:54:59 +0900

    최솟값만들기
    A벡터와 B벡터의 길이는 항상 같다. 따라서 각 배열에서 가장 큰 값이 가장 작은값과 곱해지도록 한후,
    해당 값들을 더하면 최솟값을 얻을 수 있다.

---
+++ 최솟값만들기.cpp
@@ -0,0 +1,19 @@
+//프로그래머스 최솟값 만들기
+//정렬 
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+    int size = A.size();
+    sort(A.begin(), A.end());
+    sort(B.begin(),B.end());
+    
+    for(int i=0;i<size;i++){
+        answer+=A[i]*B[size-i-1];
+    }
+    return answer;
+}
