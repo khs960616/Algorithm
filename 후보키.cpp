93b849a9650894e62eed58c86800b8fa5fa09395
khs960616 <55014803+khs960616@users.noreply.github.com>
2022-05-06 05:27:36 +0900

    조합 알고리즘
    조합 알고리즘 좀 더 응용문제 관련 연습이 좀 더 필요할듯..

---
+++ 후보키.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<bool> isVisit(8, false);
+vector<int> selectedColumn(8, 0);
+
+/*
+2019 KAKAO BLIND RECRUITMENT
+https://programmers.co.kr/learn/courses/30/lessons/42890
+
+DFS를 이용한 조합 알고리즘 
+최소성 조건 
+
+---
+
+*/
+
+void dfs(const vector<vector<string>>& relation,
+        set<string>& candidateKey,
+        const int columnSize,
+        const int cnt,
+        const int target) {
+    
+    if(cnt == target) {
+        //유일성 체크
+        set<string> s;
+        for(vector<string> r: relation) {
+            string concatString = "";
+            for(int i=0; i<cnt; ++i) {
+                concatString += r[selectedColumn[i]];
+            }
+            s.insert(concatString);
+        }
+        if(relation.size() != s.size()) return;
+        
+        //키 값 생성 
+        string key = "";
+        for(int i=0; i<cnt; ++i) {
+            key+=to_string(selectedColumn[i]);
+        }
+
+        //키가 최소성을 만족하는지 체크 (이전에 키값으로 들어간 키의 char를 하나씩 뜯어서 만일 현재 키값에 해당값들이 다 포함되어있다면 최소성을 만족하지 않음)
+        for(string c: candidateKey) {
+            int keySize = c.size();
+            int cnt=0;
+            for(char ch: c) {
+                if(key.find(ch) != std::string::npos) ++cnt;
+                if(keySize == cnt) return;
+            }
+        }
+        candidateKey.insert(key);
+        return;
+    }
+    
+    //조합
+    for(int i=0; i<columnSize; ++i) {
+        if(cnt!=0 && selectedColumn[cnt-1] >= i) continue;
+        
+        if(!isVisit[i]) {
+            isVisit[i] = true;
+            selectedColumn[cnt] = i;
+            dfs(relation, candidateKey, columnSize, cnt+1, target);
+            isVisit[i] = false;
+        }
+    }
+}
+
+int solution(vector<vector<string>> relation) {
+    set<string> candidateKey;
+    
+    int columnSize = relation[0].size();
+    for(int i=1; i<=columnSize; ++i) {
+        dfs(relation, candidateKey, columnSize, 0, i);
+    }
+
+    return candidateKey.size();
+}
