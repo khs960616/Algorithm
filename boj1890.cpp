d392a6e68192bb8c2b81699995c5d01fd250486e
khs960616 <55014803+khs960616@users.noreply.github.com>
2022-05-24 17:14:58 +0900

    DP, Memoization

--- boj1890.cpp
+++ boj1890.cpp
@@ -4,12 +4,12 @@ using namespace std;
 /*
 https://www.acmicpc.net/problem/1890
 점프
------------
 DP, Memoization
+-----------
 */
 int n;
 vector<vector<int>> v;
-vector<vector<long long>> cnt;  //원점에서 cnt[i][j] 까지 올수있는 경우의 수 
+vector<vector<long long>> cnt;  // 원점에서 cnt[i][j] 까지 올수있는 경우의 수 
 
 long long calcCnt(int y, int x) {
     long long& ret = cnt[y][x];
@@ -17,10 +17,12 @@ long long calcCnt(int y, int x) {
     ret = 0;
 
     for(int i=0; i<y; ++i) {
-        if((i + v[i][x]) == y) ret += calcCnt(i, x);
+        //같은 x축선에서, 현재 좌표의 위쪽에 있는 좌표 중, 현재 좌표로 올 수 있는 경우
+        if((i + v[i][x]) == y) ret += calcCnt(i, x); 
     } 
 
     for(int i=0; i<x; ++i) {
+        //같은 y축선에서, 현재 좌표의 왼쪽에 있는 좌표 중, 현재 좌표로 올 수 있는 경우
         if((i + v[y][i]) == x) ret += calcCnt(y, i);
     }
     return ret;
