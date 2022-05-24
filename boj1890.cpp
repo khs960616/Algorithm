56e083b1c75d40b11ad5a3699396441bde73c211
khs960616 <55014803+khs960616@users.noreply.github.com>
2022-05-24 17:12:35 +0900

    Create boj1890.cpp

---
+++ boj1890.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+https://www.acmicpc.net/problem/1890
+점프
+-----------
+DP, Memoization
+*/
+int n;
+vector<vector<int>> v;
+vector<vector<long long>> cnt;  //원점에서 cnt[i][j] 까지 올수있는 경우의 수 
+
+long long calcCnt(int y, int x) {
+    long long& ret = cnt[y][x];
+    if(ret != -1) return ret;
+    ret = 0;
+
+    for(int i=0; i<y; ++i) {
+        if((i + v[i][x]) == y) ret += calcCnt(i, x);
+    } 
+
+    for(int i=0; i<x; ++i) {
+        if((i + v[y][i]) == x) ret += calcCnt(y, i);
+    }
+    return ret;
+}
+
+int main() {
+    cin >> n;
+    v = vector<vector<int>> (n, vector<int>(n, 0));
+    cnt = vector<vector<long long>>(n, vector<long long>(n, -1));
+    cnt[0][0] = 1;
+
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<n; ++j) {
+            cin >> v[i][j];
+        }
+    }
+
+    cout << calcCnt(n-1, n-1);
+    return 0;
+}
