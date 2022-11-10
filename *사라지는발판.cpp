95b86fd61df52f0b74c96ec103813e3dc4404137
khs960616 <55014803+khs960616@users.noreply.github.com>
2022-11-10 16:28:59 +0900

    복잡한 구현[복습1]

--- *사라지는발판.cpp
+++ *사라지는발판.cpp
@@ -79,3 +79,92 @@ int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) {
     pair<bool, int> result = play(board, aloc[0], aloc[1], bloc[0], bloc[1]);
     return result.second;
 }
+
+
+/*
+
+다시 풀어보며 느낀점
+1. 변수 네이밍좀 잘하자.. 짜면서 헷갈림
+2. 재귀적으로 짜는 것이, 콜스택이 터지는게 아니면 구현 난이도면에서 확실히 훨씬 쉬워지는 것 같음
+   - 재귀함수로 구현하게 되면 
+      해당 문제에서는 결국 종료 조건(움직일 수 없는 경우 또는 현재 나의 발판이 0인 경우)를 기준으로 잘 종료시키고
+      이동이 가능한 칸으로 움직이는 것만 잘 정의하면 됨
+      
+3.     if(!result.first) {
+            flag = true;
+            min_turn = min(min_turn, result.second+1);
+        } else max_turn = max(max_turn, result.second+1);
+        
+        해당 부분 구현하면서 삽질을 좀 많이함.   
+        실제 상대방이 진 경우에만, 내가 이긴 것이고,  상대방이 이긴 케이스에는 내가 진 케이스이므로 
+        각각 min_turn과 max_turn은 항상 다르게 업데이트 해주어야함 
+        (다시 구현하면서 그냥 flag변수 하나 두고, 
+          해당 값이 true면 모든 결과를 min_turn에 업데이트했는데 이렇게되면 값 업데이트에 문제가 생기므로 주의하자.
+          
+
+#include <bits/stdc++.h>
+#define EMPTY 0
+#define NON_EMPTY 1
+using namespace std;
+
+int N, M;
+vector<pair<int, int> > dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+bool is_valid(int y, int x) {
+    return (y>=0 && y<N) && (x>=0 && x<M);
+}
+
+bool can_move(vector<vector<int>>& board, int y, int x) {
+    for(pair<int, int> dir: dirs) {
+        int ny = y+dir.first;
+        int nx = x+dir.second;
+        
+        if(!is_valid(ny, nx) || board[ny][nx] == EMPTY) continue;
+        return true;
+    }
+    return false;
+}
+
+//나의 승리 여부, 승리시 최소 count, 패배시 최대 count
+pair<bool, int> play(vector<vector<int>>& board, 
+                     int current_turn_y, int current_turn_x,
+                     int next_turn_y, int next_turn_x) {
+    
+    if(!can_move(board, current_turn_y, current_turn_x)) {
+        return {false, 0};
+    }
+    if(board[current_turn_y][current_turn_x] == EMPTY) return {false, 0};
+
+    bool flag = false;
+    int min_turn = INT_MAX, max_turn = 0;
+    for(pair<int, int> dir: dirs) {
+        int ny = current_turn_y + dir.first;
+        int nx = current_turn_x + dir.second;
+        
+        if(!is_valid(ny, nx) || board[ny][nx] == EMPTY) continue;
+        
+        board[current_turn_y][current_turn_x] = EMPTY;
+        pair<bool, int> result = play(board, next_turn_y, next_turn_x, ny, nx);
+        board[current_turn_y][current_turn_x] = NON_EMPTY;
+        
+        if(!result.first) {
+            flag = true;
+            min_turn = min(min_turn, result.second+1);
+        } else max_turn = max(max_turn, result.second+1);
+    }    
+
+    return {flag, flag ? min_turn : max_turn};
+}
+
+int solution(vector<vector<int>> board, 
+             vector<int> aloc, 
+             vector<int> bloc) {
+    
+    N = board.size();
+    M = board[0].size();
+    
+    pair<bool, int> result = play(board, aloc[0], aloc[1], bloc[0], bloc[1]);
+    return result.second;
+}
+
+*/
